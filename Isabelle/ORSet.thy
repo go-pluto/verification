2c7bda2f6b6a09fcd6c2ca4e27553dc5d70c16b6
Lennart Oldenburg <hello@lennartoldenburg.de>
2017-05-26 16:14:11 +0200

    First efforts to reformat and uniformly structure proof code.

--- Isabelle/ORSet.thy
+++ Isabelle/ORSet.thy
@@ -1,30 +1,30 @@
 theory ORSet
 imports Main
-    
+
 begin
 
 type_synonym 'a orset = "('a \<times> nat) set"
-	
+
 definition abstractState ::
   "'a orset \<Rightarrow> 'a set"
 where
   "abstractState S = {e . (\<exists> n . (e,n) \<in> S)}"
-  
+
 definition fresh ::
   "nat \<Rightarrow> 'a orset \<Rightarrow> bool"
 where
   "fresh n S = (\<nexists> e . (e,n) \<in> S)"
-  
+
 definition lookup ::
   "'a \<Rightarrow> 'a orset \<Rightarrow> bool"
 where
-  "lookup e S = (\<exists> n. (e,n) \<in> S)" 
+  "lookup e S = (\<exists> n. (e,n) \<in> S)"
 
 definition add ::
   "'element \<Rightarrow> nat \<Rightarrow> 'element orset \<Rightarrow> 'element orset"
 where
   "add e alpha S = S \<union> {(e, alpha)}"
-  
+
 definition remove_atSource ::
 	"'element \<Rightarrow> 'element orset \<Rightarrow> 'element orset"
 	where
@@ -33,7 +33,7 @@ definition remove_atSource ::
 definition remove_downstream ::
   "'element orset \<Rightarrow> 'element orset \<Rightarrow> 'element orset"
 where
-  "remove_downstream R S = S - R"  
+  "remove_downstream R S = S - R"
 
 lemma commAdd:
   shows "\<forall> S . n1 \<noteq> n2 \<longrightarrow> add e1 n1 (add e2 n2 S) = add e2 n2 (add e1 n1 S)"
@@ -43,7 +43,7 @@ proof clarify
   then show "add e1 n1 (add e2 n2 S) = add e2 n2 (add e1 n1 S)"
     by (simp add: Un_commute add_def)
 qed
-	
+
 lemma commRemove:
 fixes
   S :: "'a orset" and
@@ -51,7 +51,7 @@ fixes
   R2 :: "'a orset" and
   e1 :: "'a" and
   e2 :: "'a"
-assumes 
+assumes
   O1R1: " R1 = remove_atSource e1 S" and
   O2R2: " R2 = remove_atSource e2 S"
 shows "remove_downstream R2 (remove_downstream R1 S) = remove_downstream R1 (remove_downstream R2 S)"
@@ -75,4 +75,4 @@ proof -
 qed
 
 end
- 
+
